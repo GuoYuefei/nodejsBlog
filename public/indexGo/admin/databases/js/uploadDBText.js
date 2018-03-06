0223f1c0c719b0753f063fcd81b63a1cd960050a
Wing <17802598211@163.com>
2018-03-06 20:52:12 +0800

    ...

--- public/indexGo/admin/databases/js/uploadDBText.js
+++ public/indexGo/admin/databases/js/uploadDBText.js
@@ -3,7 +3,7 @@
  */
 adminApp.controller("uploadDBText",function($scope,$rootScope,$http){
 	$http.get('/admin/json/articletype').then(function(res){
-//		console.log(res);
+		console.log(res);
 		$scope.types = res.data.types[0].classification;
 		$scope.articletype=$scope.types[0];
 	},function(err){
